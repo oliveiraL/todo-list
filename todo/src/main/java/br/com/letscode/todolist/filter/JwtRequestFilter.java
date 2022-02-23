2b90c1170713c520bd390048279bebb0f9c611a5
Lucio <lucio.oliveira@zup.com.br>
2022-02-23 20:42:26 -0300

    jwt

---
+++ todo/src/main/java/br/com/letscode/todolist/filter/JwtRequestFilter.java
@@ -0,0 +1,26 @@
+package br.com.letscode.todolist.filter;
+
+import br.com.letscode.todolist.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+
+    private final JwtUtil jwtTokenUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+        var user = jwtTokenUtil.getUserFromToken(requestTokenHeader);
+    }
+}
--- todo/src/main/java/br/com/letscode/todolist/utils/JwtUtil.java
+++ todo/src/main/java/br/com/letscode/todolist/utils/JwtUtil.java
@@ -2,11 +2,18 @@ package br.com.letscode.todolist.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.function.Function;
 
+@Component
 public class JwtUtil {
 
     @Value("${jwt.secret}")
@@ -36,4 +43,17 @@ public class JwtUtil {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
+
+    public String generateToken() {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, UUID.randomUUID().toString());
+    }
+
+    //Cria o token e devine tempo de expiração pra ele
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 60000 * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
 }
